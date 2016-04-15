303b4b7505141769aacb7bd3858d5b7d6bc0833b
Shawn Farrow <Shaallfar@gmail.com>
2016-04-15 14:33:55 -0700

    added question 4

--- lab_script.js
+++ lab_script.js
@@ -11,9 +11,20 @@ function multiply(number1,number2){
 }
 
 function sumAndMultiply(number1,number2,number3){
-  var totalSum = sum(number1,number2) + number3;
-  var totalProduct = multiply(number1,number2) * number3;
+  var totalSum = sum(sum(number1,number2), number3);
+  var totalProduct = multiply(multiply(number1,number2), number3);
   console.log('\"' + number1 + ' and ' + number2 + ' and ' + number3 + ' sum to ' + totalSum + '.\"');
   console.log('\"The numbers ' + number1 + ' and ' + number2 + ' and ' + number3 + ' have a product of ' + totalProduct + '.\"');
   return [totalSum,totalProduct];
+}
+
+function sumArray(array){
+  var totalSum = 0;
+  for( var i = 0; i < array.length; i++)
+  {
+    totalSum += array[i];
+  }
+    //"2,3,4 was passed in as an array of numbers, and 9 is their sum."
+  console.table(array);
+  console.log('\"' + array + ' was passed in as an array of numbers, and ' + totalSum + ' is their sum.\"');
 }
