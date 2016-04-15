e145334c9f10764d6660d4e191023d6eb59b0f20
Shawn Farrow <Shaallfar@gmail.com>
2016-04-15 13:35:28 -0700

    added multiply function

--- lab_script.js
+++ lab_script.js
@@ -1,5 +1,11 @@
 function sum(number1,number2){
   var sum = number1 + number2;
-  console.log('\"The sum of ' + number1 + ' and ' + number2 + ' is ' + sum + '\"');
+  console.log('\"The sum of ' + number1 + ' and ' + number2 + ' is ' + sum + '.\"');
   return sum;
+}
+
+function multiply(number1,number2){
+  var product = number1 * number2;
+  console.log('\"The product of ' + number1 + ' and ' + number2 + ' is ' + product + '.\"');
+  return product;
 }
